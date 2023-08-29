b8cac24879b1a611614f43975c848a48037fc411
Alberto <71283412+albertoortegafernandez@users.noreply.github.com>
2023-08-29 17:10:04 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Alberto Ortega Fernandez
    # Input:
    # p, principal amount
    # t, time period in years
